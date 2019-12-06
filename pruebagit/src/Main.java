1691b65c955f7a897de0bd1e6ffeaaf983f4a444
magor12 <rogelio.llerenad@gmail.com>
2019-12-06 18:43:21 -0500

    correcion3567

--- pruebagit/src/Main.java
+++ pruebagit/src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println(" TE AMO " + " " + s);
 
         System.out.println(" TE AMO3 " + " " + s);
+        System.out.println(" TE AMO4 " + " " + s);
 
         
     }
