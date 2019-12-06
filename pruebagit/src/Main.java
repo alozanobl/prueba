09aaca8570249e47e51b7fdc347257545727607f
alozano <alozanoblanquicett@gmail.com>
2019-12-06 17:00:47 -0500

    se imprime una prueba

--- pruebagit/src/Main.java
+++ pruebagit/src/Main.java
@@ -15,7 +15,9 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
+        
+        
+        String p = "n";
         System.out.println("dddd");
     }
 
