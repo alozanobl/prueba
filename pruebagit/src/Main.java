7fb5b1f916cc68f7efb1ac1e2b3e92b52dc51d42
alozano <alozanoblanquicett@gmail.com>
2019-12-06 18:52:20 -0500

    Merge origin/master
    Conflicts:
    	pruebagit/src/Main.java

--- pruebagit/src/Main.java
+++ pruebagit/src/Main.java
@@ -12,8 +12,6 @@ public class Main {
 
     private static void saludar(String s) {
         System.out.println(" TE AMO " + " " + s);
-        System.out.println(" TE AMO " + " " + s);
-        System.out.println(" TE AMO " + " " + s);
     }
 
 }
