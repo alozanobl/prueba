f174f7d44d2913969e30f4b5739fd8ebbcc9f1b8
magor12 <rogelio.llerenad@gmail.com>
2019-12-06 17:21:30 -0500

    Explosion2

--- pruebagit/src/Main.java
+++ pruebagit/src/Main.java
@@ -12,7 +12,8 @@ public class Main {
 
     private static void saludar(String s) {
         System.out.println(" Hola mundo " + " " + s);
-sfñsdfjdjfkjdklfjldjflñjdkk
+        System.out.println(" Hola mundo 2" + " " + s);
+        
     }
 
 }
