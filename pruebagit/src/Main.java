e97ba443aa123611b683768e025cb27662f27a56
alozano <alozanoblanquicett@gmail.com>
2019-12-06 18:03:08 -0500

    ey cambiel la linea del metodo saludar.

--- pruebagit/src/Main.java
+++ pruebagit/src/Main.java
@@ -9,10 +9,8 @@ public class Main {
         }
 
     }
-
     private static void saludar(String s) {
         System.out.println(" Hola mundo " + " " + s);
-        System.out.println("el otro huuuuuu");
     }
 
 }
